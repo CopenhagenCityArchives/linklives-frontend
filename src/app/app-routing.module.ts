0b73e33ed839143b9d679d8511234c027b78a7d9
jfeo <jens@feodor.dk>
2020-03-26 11:05:47 +0100

    Add basic routing and navigation to app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchFreetextComponent } from './search-freetext/search-freetext.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/simple-search', pathMatch: 'full' },
+  { path: 'simple-search', component: SearchFreetextComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
