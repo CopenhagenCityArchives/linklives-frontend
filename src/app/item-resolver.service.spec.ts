3b51b0ec80c980784aa040cc00679d7f053adb55
jfeo <jens@feodor.dk>
2020-05-12 10:13:44 +0200

    Update item-resolver.service to use elasticsearch.service

--- src/app/item-resolver.service.spec.ts
+++ src/app/item-resolver.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ItemResolverService } from './item-resolver.service';
+import { ElasticsearchService } from './elasticsearch/elasticsearch.service';
+import { ActivatedRouteSnapshot } from '@angular/router';
 
 describe('ItemResolverService', () => {
   let service: ItemResolverService;
+  let elasticsearchServiceStub = {
+    doc: jasmine.createSpy('doc')
+  }
+  let activatedRouteSnapshotStub = {
+    params: {},
+    data: {}
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: ElasticsearchService, useValue: elasticsearchServiceStub },
+        { provide: ActivatedRouteSnapshot, useValue: activatedRouteSnapshotStub }
+      ]
+    });
     service = TestBed.inject(ItemResolverService);
   });
 
--- src/app/item-resolver.service.ts
+++ src/app/item-resolver.service.ts
@@ -1,39 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, ActivatedRoute } from '@angular/router';
-import { PersonalAppearance, SearchService } from './search/search.service';
-import { of, Observable, forkJoin } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+import { PersonAppearance } from './search/search.service';
+import { of } from 'rxjs';
+import { ElasticsearchService } from './elasticsearch/elasticsearch.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ItemResolverService implements Resolve<PersonalAppearance | PersonalAppearance[]> {
+export class ItemResolverService implements Resolve<PersonAppearance | PersonAppearance[]> {
 
-  constructor(private http: HttpClient, private route: ActivatedRoute, private service: SearchService) { }
+  constructor(private elasticsearch: ElasticsearchService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    
-
     if (history.state.data) {
       return of(history.state.data);
     } else {
-      console.log("Resolve 2");
-      console.log(route);
-      return this.http.get(`${environment.apiUrl}/${route.data['index']}/_doc/${route.params['id']}`);
+      return this.elasticsearch.doc(route.data['index'], route.params['id']);
     }
-/*
-    if (history.state) {
-      console.log("Resolve 2");
-      console.log(route);
-      return this.http.get(`${environment.apiUrl}/${route.data['index']}/_doc/${route.params['id']}`);
-    } else {
-      console.log("Resolve 3");
-      console.log(state.url);
-      console.log(route);
-      return this.http.get(`${environment.apiUrl}/${route.data['index']}/_doc/${route.params['id']}`)
-    }*/
   }
   
 }
