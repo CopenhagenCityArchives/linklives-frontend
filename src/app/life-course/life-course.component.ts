5d0fa4a7b6988f21b99a47f2df07240fedcb309e
Anders Enghøj <anders@deranged.dk>
2020-11-16 12:55:45 +0100

    Lifecourse view layouting and data

--- src/app/life-course/life-course.component.ts
+++ src/app/life-course/life-course.component.ts
@@ -12,6 +12,31 @@ export class LifeCourseComponent implements OnInit {
   pas: PersonAppearance[] = [];
   lifecourseId: number;
 
+  get latestPersonAppearance() {
+    const sortedByYear = this.pas.sort(function(a, b) {
+      if (a.source_year > b.source_year) {
+        return 1;
+      }
+      if (a.source_year < b.source_year) {
+        return -1;
+      }
+      return 0;
+    });
+    return sortedByYear[sortedByYear.length - 1];
+  }
+
+  get sourceLocation() {
+    return [
+      ...new Set(
+        [
+          this.latestPersonAppearance.parish,
+          this.latestPersonAppearance.district,
+          this.latestPersonAppearance.county
+        ].filter((x) => x)
+      )
+    ].join(", ");
+  }
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
