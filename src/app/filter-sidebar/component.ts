167997344560ee24b6d1ac11e4f77828614e2a64
Anders Enghøj <anders@deranged.dk>
2020-11-26 16:24:27 +0100

    active filters is now visible in filter sidebar

--- src/app/filter-sidebar/component.ts
+++ src/app/filter-sidebar/component.ts
@@ -25,14 +25,17 @@ export interface Option {
 export class FilterSidebar implements OnInit {
   @Input() featherIconPath: string;
   @Input() possibleYears: Array<number>;
+  @Input() sourceFilter: Array<number>;
   @Input() name: string;
   @Input() openSidebar: boolean;
   @Output() closeSidebar: EventEmitter<any> = new EventEmitter();
   @Output() change: EventEmitter<object> = new EventEmitter<object>();
+  @Output() removeFilter: EventEmitter<any> = new EventEmitter();
 
   constructor(private elRef: ElementRef) {}
 
   isOpen: boolean = false;
+  filters = [];
 
   open() {
     this.openSidebar = true;
@@ -43,11 +46,20 @@ export class FilterSidebar implements OnInit {
   }
 
   addFilter(option) {
-    this.change.emit(option);
+    const chosenOption = this.filters.find(({value}) => value === option.value);
+    
+    if(this.sourceFilter.find(x => x === chosenOption.value)) {
+      chosenOption.chosen = false;
+      this.removeFilter.emit(option.value);
+      return;
+    }
+
+    chosenOption.chosen = true;
+    this.change.emit(option.value);
   }
 
   ngOnInit(): void {
-
+    this.filters = this.possibleYears.map(x => ({ "label": `Folketælling ${x}`, "value": x, "chosen": false }));
   }
 
 }
