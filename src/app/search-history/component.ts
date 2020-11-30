73a272a3114c1e76685ddb3180db2e337f913247
Anders Enghøj <anders@deranged.dk>
2020-11-30 16:18:34 +0100

    extract sidebar styling. Add more data to search history UI.

--- src/app/search-history/component.ts
+++ src/app/search-history/component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { getSearchHistory, onSearchHistoryEntry, SearchHistoryEntry } from '../search-history';
 
 @Component({
   selector: 'app-search-history',
   templateUrl: './component.html',
 })
+
 export class SearchHistoryComponent implements OnInit {
+  @Input() openSearchHistory: boolean;
+  @Input() featherIconPath: string;
+  @Output() close: EventEmitter<any> = new EventEmitter();
   searchHistory: SearchHistoryEntry[] = getSearchHistory();
 
   ngOnInit(): void {
     onSearchHistoryEntry((history) => this.searchHistory = history);
+    console.warn("searchHistory", this.searchHistory)
+  }
+
+  closeSearchHistory() {
+    this.close.emit(null);
   }
 }
