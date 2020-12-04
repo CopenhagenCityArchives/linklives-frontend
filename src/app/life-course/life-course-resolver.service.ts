c57a5331f939efc4a37af5be8026e68ace2691bb
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2020-12-04 16:02:17 +0100

    Reorganize life course resolver code to happen inline

--- src/app/life-course/life-course-resolver.service.ts
+++ src/app/life-course/life-course-resolver.service.ts
@@ -14,34 +14,34 @@ export class LifeCourseResolverService implements Resolve<{lifecourseId:number, 
   constructor(private elasticsearch: ElasticsearchService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<{ lifecourseId: number; personAppearances: PersonAppearance[]; links: Link[]; }> {
-    const x = (pas) => this.elasticsearch.searchLinks(pas)
-      .pipe(map((linksResult, index) => {
-        const paIds: string[] = pas.map((pa) => `${pa.source_id}-${pa.pa_id}`);
-        const links = linksResult.filter((link) => {
-          const linkStartId = `${link.source_id1}-${link.pa_id1}`;
-          const linkEndId = `${link.source_id2}-${link.pa_id2}`;
-          return paIds.includes(linkStartId) && paIds.includes(linkEndId);
-        });
+    const lifecourseId = route.params['id'];
 
-        return {
-          lifecourseId: route.params['id'],
-          personAppearances: pas as PersonAppearance[],
-          links,
-        };
-      }));
-
-    return this.elasticsearch.getDocument('lifecourses', route.params['id'])
+    return this.elasticsearch.getDocument('lifecourses', lifecourseId)
       .pipe(map(pas => pas as PersonAppearance[]))
       .pipe(mergeMap((pas: PersonAppearance[], index) => {
         addSearchHistoryEntry({
           type: SearchHistoryEntryType.Lifecourse,
           lifecourse: {
-            id: route.params['id'],
+            id: lifecourseId,
             personAppearances: pas,
           },
         });
 
-        return x(pas);
+        return this.elasticsearch.searchLinks(pas)
+          .pipe(map((linksResult, index) => {
+            const paIds: string[] = pas.map((pa) => `${pa.source_id}-${pa.pa_id}`);
+            const links = linksResult.filter((link) => {
+              const linkStartId = `${link.source_id1}-${link.pa_id1}`;
+              const linkEndId = `${link.source_id2}-${link.pa_id2}`;
+              return paIds.includes(linkStartId) && paIds.includes(linkEndId);
+            });
+
+            return {
+              lifecourseId,
+              personAppearances: pas,
+              links,
+            };
+          }));
       }));
   }
 }
