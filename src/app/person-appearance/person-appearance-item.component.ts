2e55cddf87f6417130ec62271c9397b5419d3a59
Anders Enghøj <anders@deranged.dk>
2020-12-08 11:32:21 +0100

    Add functions to compute PA display fields: names, roles, occupation

--- src/app/person-appearance/person-appearance-item.component.ts
+++ src/app/person-appearance/person-appearance-item.component.ts
@@ -40,6 +40,50 @@ export class PersonAppearanceItemComponent implements OnInit {
     return prettySourceLocation(this.personAppearance);
   }
 
+  get personName() {
+    return this.personAppearance.name_clean || `${this.personFirstName()} ${this.personLastName()}`;
+  }
+
+  get personRole() {
+    if(this.personAppearance.role != "unknown" || !this.personAppearance.role) {
+      return this.personAppearance.role;
+    }
+
+    if(this.personAppearance.household_position_std != "unknown" || !this.personAppearance.household_position_std) {
+      return this.personAppearance.household_position_std;
+    }
+
+    if(this.personAppearance.household_position != "unknown" || !this.personAppearance.household_position) {
+      return this.personAppearance.household_position;
+    }
+
+    return "";
+  }
+
+  get occupation() {
+    return this.personAppearance.occupation || this.personAppearance.positions || "";
+  }
+
+  personFirstName() {
+    if(this.personAppearance.first_names_clean) {
+      return this.personAppearance.first_names_clean;
+    }
+    if(this.personAppearance.first_names.length) {
+      return this.personAppearance.first_names.join(" ");
+    }
+    return "";
+  }
+
+  personLastName() {
+    if(this.personAppearance.lastname_clean) {
+      return this.personAppearance.lastname_clean;
+    }
+    if(this.personAppearance.patronyms.length) {
+      return this.personAppearance.patronyms.join(" ");
+    }
+    return "";
+  }
+
   constructor() { }
 
   ngOnInit(): void {
