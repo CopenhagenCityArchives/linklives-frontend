a4ab00cc420a5a99322e59715dc8fabc5dc8908d
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2020-12-10 11:32:56 +0100

    Field selection for source data selected by event type

--- src/app/person-appearance/source-data.component.ts
+++ src/app/person-appearance/source-data.component.ts
@@ -15,43 +15,46 @@ export class SourceDataComponent implements OnInit {
   source: Source;
 
   standardizedDataFields = {
-    id: "id",
-    event_type: "event_type",
-    role: "role",
-    name_clean: "name_clean",
-    name_std: "name_std",
-    first_names: "first_names",
-    family_names: "family_names",
-    patronyms: "patronyms",
-    uncat_names: "uncat_names",
-    maiden_family_names: "maiden_family_names",
-    maiden_patronyms: "maiden_patronyms",
-    all_possible_family_names: "all_possible_family_names",
-    all_possible_patronyms: "all_possible_patronyms",
-    marital_status_clean: "marital_status_clean",
-    marital_status_std: "marital_status_std",
-    household_position_std: "household_position_std",
-    gender_clean: "gender_clean",
-    gender_std: "gender_std",
-    age_clean: "age_clean",
-    hh_id: "hh_id",
-    full_address: "full_address",
-    birth_place_clean: "birth_place_clean",
-    birth_place_parish: "birth_place_parish",
-    birth_place_district: "birth_place_district",
-    birth_place_county: "birth_place_county",
-    birth_place_koebstad: "birth_place_koebstad",
-    birth_place_town: "birth_place_town",
-    birth_place_place: "birth_place_place",
-    birth_place_island: "birth_place_island",
-    birth_place_other: "birth_place_other",
-    birth_place_parish_std: "birth_place_parish_std",
-    birth_place_county_std: "birth_place_county_std",
-    birth_place_koebstad_std: "birth_place_koebstad_std",
+    census: {
+      id: "id",
+      event_type: "event_type",
+      role: "role",
+      name_clean: "name_clean",
+      name_std: "name_std",
+      first_names: "first_names",
+      family_names: "family_names",
+      patronyms: "patronyms",
+      uncat_names: "uncat_names",
+      maiden_family_names: "maiden_family_names",
+      maiden_patronyms: "maiden_patronyms",
+      all_possible_family_names: "all_possible_family_names",
+      all_possible_patronyms: "all_possible_patronyms",
+      marital_status_clean: "marital_status_clean",
+      marital_status_std: "marital_status_std",
+      household_position_std: "household_position_std",
+      gender_clean: "gender_clean",
+      gender_std: "gender_std",
+      age_clean: "age_clean",
+      hh_id: "hh_id",
+      full_address: "full_address",
+      birth_place_clean: "birth_place_clean",
+      birth_place_parish: "birth_place_parish",
+      birth_place_district: "birth_place_district",
+      birth_place_county: "birth_place_county",
+      birth_place_koebstad: "birth_place_koebstad",
+      birth_place_town: "birth_place_town",
+      birth_place_place: "birth_place_place",
+      birth_place_island: "birth_place_island",
+      birth_place_other: "birth_place_other",
+      birth_place_parish_std: "birth_place_parish_std",
+      birth_place_county_std: "birth_place_county_std",
+      birth_place_koebstad_std: "birth_place_koebstad_std",
+    },
+    burial: {},
   };
 
   get standardizedDataLines() {
-    return Object.keys(this.standardizedDataFields)
+    return Object.keys(this.standardizedDataFields[this.pa.event_type])
       .filter((key) => {
         const isUndef = typeof this.pa[key] === "undefined";
         if(isUndef) {
@@ -59,20 +62,23 @@ export class SourceDataComponent implements OnInit {
         }
         return !isUndef;
       })
-      .map((key) => ({ label: this.standardizedDataFields[key], value: this.pa[key] }));
+      .map((key) => ({ label: this.standardizedDataFields[this.pa.event_type][key], value: this.pa[key] }));
   }
 
   sourceDataFields = {
-    source_id: "Kilde ID",
-    source_year: "Kildeår",
-    source_reference: "source_reference",
-    transcription_code: "transcription_code",
-    transcription_id: "transcription_id",
-    household_family_no: "Husstands nr.",
+    census: {
+      source_id: "Kilde ID",
+      source_year: "Kildeår",
+      source_reference: "source_reference",
+      transcription_code: "transcription_code",
+      transcription_id: "transcription_id",
+      household_family_no: "Husstands nr.",
+    },
+    burial: {},
   };
 
   get sourceDataLines() {
-    return Object.keys(this.sourceDataFields)
+    return Object.keys(this.sourceDataFields[this.pa.event_type])
       .filter((key) => {
         const isUndef = typeof this.pa[key] === "undefined";
         if(isUndef) {
@@ -80,18 +86,22 @@ export class SourceDataComponent implements OnInit {
         }
         return !isUndef;
       })
-      .map((key) => ({ label: this.sourceDataFields[key], value: this.pa[key] }));
+      .map((key) => ({ label: this.sourceDataFields[this.pa.event_type][key], value: this.pa[key] }));
   }
 
   excludeDataFields = [
     "id",
     "pa_id",
+    "first_names_sortable",
+    "family_names_sortable",
+    "last_updated",
+    "pa_entry_permalink",
   ];
 
   get otherDataLines() {
-    const alreadyUsedFields = Object.keys(this.standardizedDataFields)
-      .concat(Object.keys(this.sourceDataFields))
-      .concat(Object.keys(this.originalDataFields))
+    const alreadyUsedFields = Object.keys(this.standardizedDataFields[this.pa.event_type])
+      .concat(Object.keys(this.sourceDataFields[this.pa.event_type]))
+      .concat(Object.keys(this.originalDataFields[this.pa.event_type]))
       .concat(this.excludeDataFields);
 
     return Object.keys(this.pa)
@@ -100,27 +110,30 @@ export class SourceDataComponent implements OnInit {
   }
 
   originalDataFields = {
-    name: "Navn",
-    occupation: "Erhverv",
-    age: "Alder",
-    gender: "Køn",
-    birth_place: "Fødested",
-    household_position: "Stilling i husstanden",
-    marital_status: "Civilstand",
-    address: "? (address)",
-    place_name: "Stednavn",
-    land_register_address: "? (land_register_address)",
-    land_register: "? (land_register)",
-    parish: "Bopæl sogn",
-    district: "Bopæl herred",
-    county: "Bopæl amt",
-    parish_type: "parish_type",
-    state_region: "Land",
-    transcriber_comments: "Indtasters kommentar",
+    census: {
+      name: "Navn",
+      occupation: "Erhverv",
+      age: "Alder",
+      gender: "Køn",
+      birth_place: "Fødested",
+      household_position: "Stilling i husstanden",
+      marital_status: "Civilstand",
+      address: "? (address)",
+      place_name: "Stednavn",
+      land_register_address: "? (land_register_address)",
+      land_register: "? (land_register)",
+      parish: "Bopæl sogn",
+      district: "Bopæl herred",
+      county: "Bopæl amt",
+      parish_type: "parish_type",
+      state_region: "Land",
+      transcriber_comments: "Indtasters kommentar",
+    },
+    burial: {}
   };
 
   get originalDataLines() {
-    return Object.keys(this.originalDataFields)
+    return Object.keys(this.originalDataFields[this.pa.event_type])
       .filter((key) => {
         const isUndef = typeof this.pa[key] === "undefined";
         if(isUndef) {
@@ -128,7 +141,7 @@ export class SourceDataComponent implements OnInit {
         }
         return !isUndef;
       })
-      .map((key) => ({ label: this.originalDataFields[key], value: this.pa[key] }));
+      .map((key) => ({ label: this.originalDataFields[this.pa.event_type][key], value: this.pa[key] }));
   }
 
   ngOnInit(): void {
