720bd36638be15ac19174329bf3450f6f97dd2f2
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2021-02-09 16:45:38 +0100

    Remove now unused search term values

--- src/app/search-term-values.ts
+++ src/app/search-term-values.ts
@@ -21,51 +21,6 @@ export const mapSearchKeys = {
   sourceYear: "source_year",
 };
 
-export const mapQueryShouldKey = {
-  firstName: [
-    "first_names",
-    "first_names_clean",
-    "first_names_std",
-  ],
-  birthPlace: [
-    "birth_place",
-    "birth_place_clean",
-    "birth_place_county",
-    "birth_place_county_std",
-    "birth_place_district",
-    "birth_place_island",
-    "birth_place_koebstad",
-    "birth_place_koebstad_std",
-    "birth_place_other",
-    "birth_place_parish",
-    "birth_place_parish_std",
-    "birth_place_place",
-    "birth_place_town",
-  ],
-  lastName: [
-    "all_possible_family_names",
-    "all_possible_patronyms",
-    "family_names",
-    "patronyms",
-    "lastname_clean",
-  ],
-  birthName: [
-    "maiden_family_names",
-    "maiden_patronyms",
-    "birthname_clean",
-  ],
-  sourcePlace: [
-    "parish",
-    "county",
-    "district",
-  ],
-  maritalStatus: [
-    "marital_status",
-    "marital_status_clean",
-    "marital_status_std",
-  ],
-};
-
 export const sortValues = {
   relevance: [ "_score" ],
   firstName: [ "first_names_sortable" ],
