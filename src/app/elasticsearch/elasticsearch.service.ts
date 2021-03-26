730b8f3ca096326d06058ff051c126c981a4e3fc
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2021-03-26 12:34:42 +0100

    Disable fuzziness for now

--- src/app/elasticsearch/elasticsearch.service.ts
+++ src/app/elasticsearch/elasticsearch.service.ts
@@ -394,8 +394,11 @@ export class ElasticsearchService {
               [`person_appearance.${searchKey}`]: {
                 // Match query splits into terms on space, so we can simplify the query here
                 query: nonWildcardTerms.join(" "),
-                fuzziness: "AUTO",
-                max_expansions: 250,
+
+                // Fuzziness disabled for now, until we figure out how exactly we want it to work
+                fuzziness: 0, //"AUTO",
+                //max_expansions: 250,
+
                 operator: "AND"
               }
             }
