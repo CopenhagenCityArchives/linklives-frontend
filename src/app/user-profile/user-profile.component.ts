f0a08cbcb5200f0b871b44dcdab4f43a6183098a
Philip <philip@deranged.dk>
2021-06-01 15:48:47 +0200

    WIP: let users edit their info

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -9,14 +9,19 @@ import { AuthService } from '@auth0/auth0-angular';
 
 export class UserProfilePage {
   constructor(public auth: AuthService) {}
+  isEditingProfile:boolean = true;
 
   get config() {
     return window["lls"];
   };
 
+  editProfile() {
+    this.isEditingProfile = true;
+  };
+
   logout(){
     this.auth.logout();
-  }
+  };
 
   featherSpriteUrl = this.config.featherIconPath;
 }
