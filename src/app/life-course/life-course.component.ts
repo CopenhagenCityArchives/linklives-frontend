af7db86428c9be42047468aeebe157f37ba24aa8
Helena Bach <helena@deranged.dk>
2021-08-12 14:35:28 +0200

    get lifecourse by key using the API. To do this, we are now using key property of lifecouse to identify the lifecourse. Get lifecouse endpoint is including the links in the lifecourse, therefore we dont need to get theese after getting the lifecourse

--- src/app/life-course/life-course.component.ts
+++ src/app/life-course/life-course.component.ts
@@ -13,7 +13,7 @@ import { ElasticsearchService } from '../elasticsearch/elasticsearch.service';
 export class LifeCourseComponent implements OnInit {
 
   pas: PersonAppearance[] = [];
-  lifecourseId: number;
+  lifecourseKey: string;
   links: Link[];
   getLatestSearchQuery = getLatestSearchQuery;
 
@@ -173,7 +173,7 @@ export class LifeCourseComponent implements OnInit {
   ngOnInit(): void {
     this.route.data.subscribe(next => {
       this.pas = next.lifecourse.personAppearances as PersonAppearance[];
-      this.lifecourseId = next.lifecourse.lifecourseId;
+      this.lifecourseKey = next.lifecourse.lifecourseKey;
       this.links = next.lifecourse.links;
     });
   }
