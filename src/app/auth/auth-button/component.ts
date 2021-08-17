135d85440e6d7237a9231eb4db1b518e654a31b4
Helena Bach <helena@deranged.dk>
2021-08-17 15:59:50 +0200

    refactor auth btn

--- src/app/auth/auth-button/component.ts
+++ src/app/auth/auth-button/component.ts
@@ -4,29 +4,7 @@ import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'lls-auth-button',
-  template: `
-    <ng-container *ngIf="auth.isAuthenticated$ | async; else loggedOut">
-      <a class="lls-link" routerLink="/my-page">
-        Gå til min side
-      </a>
-      <a
-        (click)="auth.logout({ returnTo: document.location.origin })"
-        class="lls-link u-ml-4"
-      >
-        Log ud
-      </a>
-    </ng-container>
-
-    <ng-template #loggedOut>
-      <a
-        (click)="login()"
-        class="lls-link"
-      >
-        Log ind
-      </a>
-    </ng-template>
-  `,
-  styles: [],
+  templateUrl: './component.html',
 })
 
 export class AuthButtonComponent {
