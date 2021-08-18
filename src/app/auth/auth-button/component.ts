59b647ffe15bd89de0d4f087f0cfcdc8d17945c7
Helena Bach <helena@deranged.dk>
2021-08-18 12:09:28 +0200

    refactor handleLogin

--- src/app/auth/auth-button/component.ts
+++ src/app/auth/auth-button/component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
-import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
+import { AuthService } from '@auth0/auth0-angular';
+import { AuthUtil } from '../util';
 
 @Component({
   selector: 'lls-auth-button',
@@ -9,20 +10,9 @@ import { DOCUMENT } from '@angular/common';
 
 export class AuthButtonComponent {
   window = window;
-  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) { }
+  constructor(@Inject(DOCUMENT) public document: Document, private authUtil: AuthUtil, public auth: AuthService) { }
 
   login() {
-    const onLoginCompleted = {
-      path: window.location.pathname,
-    };
-
-    if(window.location.search.length > 1) {
-      onLoginCompleted['query'] = window.location.search.substring(1);
-    }
-    localStorage.setItem('onLoginCompleted', JSON.stringify(onLoginCompleted));
-
-    this.auth.loginWithRedirect({
-      appState: { target: 'login-completed' }
-    })
+    this.authUtil.handleLogin();
   }
 }
