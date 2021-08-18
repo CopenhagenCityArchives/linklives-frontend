08460c35c5b5895bbee1976839d043e6a33d01aa
Helena Bach <helena@deranged.dk>
2021-08-18 14:14:24 +0200

    get correct target url

---
+++ src/app/auth/util.ts
@@ -0,0 +1,27 @@
+import { AuthService } from '@auth0/auth0-angular';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthUtil {
+  constructor(public auth: AuthService) { }
+
+  handleLogin() {
+    const onLoginCompleted = {
+      path: window.location.pathname,
+    };
+  
+    if(window.location.search.length > 1) {
+      onLoginCompleted['query'] = window.location.search.substring(1);
+    }
+    localStorage.setItem('onLoginCompleted', JSON.stringify(onLoginCompleted));
+  
+    console.log('now lets call auth service!');
+    this.auth.loginWithRedirect({
+      appState: { target: 'find-livsforloeb-testversion/login-completed/' }
+      //redirect_uri: 'https://link-lives.dk/find-livsforloeb-testversion/login-completed',// window.location.href
+    })
+  }
+}
