8cf77b733d64bb1de89bfba088b6ef7486edaa6a
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2021-08-23 13:03:30 +0200

    Production use production API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://api-test.link-lives.dk",
+  apiUrl: "https://api.link-lives.dk",
   // TODO: use the pathPrefix with travis is setup to prod/staging
   pathPrefix: '/soeg-i-livsforloeb-og-kilder/',
 };
