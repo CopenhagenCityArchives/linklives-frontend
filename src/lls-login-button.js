4c30ff4fd8ba8b9290b01b31c6b2f2242e8d078e
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2022-05-16 16:51:20 +0200

    Add path prefix to links added to menun

--- src/lls-login-button.js
+++ src/lls-login-button.js
@@ -1,4 +1,14 @@
 (function() {
+  let pathPrefix = '';
+  try {
+    pathPrefix = process.env.PATH_PREFIX;
+  }
+  catch(error) {
+    // No path prefix
+  }
+
+  console.log("pref", pathPrefix);
+
   const loggedIn = localStorage.getItem("lls__isLoggedIn");
 
   const nav = document.getElementById('menu-dansk-language-switcher');
