aef4758660f9ac14bad279a46748fe101efb6b08
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2022-07-12 12:52:34 +0200

    Use production APIs in staging -- staging env has been closed

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: "https://api-test.link-lives.dk",
-  esUrl: "https://data-dev.link-lives.dk",
+  apiUrl: "https://api.link-lives.dk",
+  esUrl: "https://data.link-lives.dk",
   pathPrefix: "/find-livsforloeb-testversion",
   auth0ClientId: '7lYbAwzUER3epciKfadgIoO8LUmhIk5x',
 };
