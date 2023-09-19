80a903d38281bb93149da78c666040e2ec4e47b3
Anders Enghøj <andersenghoej@gmail.com>
2023-09-19 14:08:11 +0200

    Update ga -> gtag

--- src/app/analytics.module.ts
+++ src/app/analytics.module.ts
@@ -4,7 +4,7 @@ import { filter } from "rxjs/operators";
 
 type GoogleAnalyticsFunction = (command: string, field: string, other?: any) => void;
 type WindowWithGoogleAnalytics = Window & typeof globalThis & {
-  ga: GoogleAnalyticsFunction,
+  gtag: GoogleAnalyticsFunction,
 };
 
 @NgModule()
@@ -17,10 +17,10 @@ export class AnalyticsModule {
           // Google Analytics has not been set on `window` on first request,
           // so we need to guard it and make sure it exists before we send off
           // a pageview to Google Analytics
-          const ga = (window as WindowWithGoogleAnalytics).ga;
-          if(ga) {
-            ga('set', 'page', `${location.pathname}${location.search}${location.hash}`);
-            ga('send', 'pageview');
+          const gtag = (window as WindowWithGoogleAnalytics).gtag;
+          if(gtag) {
+            gtag('set', 'page', `${location.pathname}${location.search}${location.hash}`);
+            gtag('send', 'pageview');
           }
         },
       });
