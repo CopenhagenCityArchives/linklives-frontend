9baf9d05b7cda051d9fd441114afbd7ddacd3a44
Anders Enghøj <andersenghoej@gmail.com>
2023-09-27 10:28:17 +0200

    Minor fix to analytics module

--- src/app/analytics.module.ts
+++ src/app/analytics.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { NavigationEnd, Router } from "@angular/router";
 import { filter } from "rxjs/operators";
 
-type GoogleAnalyticsFunction = (command: string, field: object|string, other?: any) => void;
+type GoogleAnalyticsFunction = (command: string, field: Record<string,any>|string, other?: any) => void;
 type WindowWithGoogleAnalytics = Window & typeof globalThis & {
   gtag: GoogleAnalyticsFunction,
 };
@@ -19,7 +19,7 @@ export class AnalyticsModule {
           // a pageview to Google Analytics
           const gtag = (window as WindowWithGoogleAnalytics).gtag;
           if(gtag) {
-            gtag('set', {"page": `${location.pathname}${location.search}${location.hash}`});
+            gtag('set', { page: `${location.pathname}${location.search}${location.hash}` });
             gtag('event', 'page_view');
           }
         },
